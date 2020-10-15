e8e5b27fbc348ada20d759a24c2fed1147fc8053
TalionSlade <arpan01ghosh1307@gmail.com>
2020-10-15 17:11:06 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          This is the second commit. Hello from v2
         </p>
         <a
           className="App-link"
